36f060c34da76fd9da841ec1d3768d2e5f71153b
patawee <psgolf16@gmail.com>
2018-02-21 12:39:20 +0700

    Object For Test

---
+++ Web/Web/Object Repository/input field search.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input field search</name>
+   <tag></tag>
+   <elementGuidId>4d5e1de1-4f80-4ff4-8838-df21491f493e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>lst-ib</value>
+   </webElementProperties>
+</WebElementEntity>
