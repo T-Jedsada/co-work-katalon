dfcabcf9e8320021c91e98627b27830908c35307
patawee <psgolf16@gmail.com>
2018-02-22 11:06:41 +0700

    Script For Test Case

---
+++ Android/Android/Scripts/Register with e-mail/RegisterEmail_TC04/Script1519271997967.groovy
@@ -0,0 +1,49 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication(GlobalVariable.APK_URL, false)
+
+Mobile.waitForElementPresent(findTestObject('Register/btn/btnRegister'), 0)
+
+Mobile.tap(findTestObject('Register/btn/btnRegister'), 0)
+
+Mobile.waitForElementPresent(findTestObject('Register/edt/edtName'), 0)
+
+Mobile.setText(findTestObject('Register/edt/edtName'), GlobalVariable.NAME_RGT, 0)
+
+Mobile.waitForElementPresent(findTestObject('Register/edt/edtEmail'), 0)
+
+Mobile.setText(findTestObject('Register/edt/edtEmail'), GlobalVariable.EMAIL_RGT, 0)
+
+Mobile.waitForElementPresent(findTestObject('Register/edt/edtPassword'), 0)
+
+Mobile.setText(findTestObject('Register/edt/edtPassword'), '12', 0)
+
+Mobile.waitForElementPresent(findTestObject('Register/btn/btnSubmit'), 0)
+
+Mobile.waitForElementPresent(findTestObject('Register/btn/btnFacebookRegister'), 0)
+
+Mobile.tap(findTestObject('Register/btn/btnSubmit'), 0)
+
+Mobile.scrollToText('', FailureHandling.STOP_ON_FAILURE)
+
+Mobile.closeApplication()
+
