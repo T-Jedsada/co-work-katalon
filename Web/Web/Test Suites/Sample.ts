8847db6875b68c031bf32e1274f6259831868228
patawee <psgolf16@gmail.com>
2018-02-22 11:07:48 +0700

    Sample Web Test Suite

---
+++ Web/Web/Test Suites/Sample.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sample</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8fc4e610-6409-4811-bc39-5e78316941fe</testSuiteGuid>
+   <testCaseLink>
+      <guid>659d8a4c-7bc2-4476-a11b-129b463e6def</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search Google</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
