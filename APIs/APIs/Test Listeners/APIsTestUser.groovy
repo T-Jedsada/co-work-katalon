1a5538818fd34c03c278aabb941512f47a4a7922
patawee <psgolf16@gmail.com>
2018-03-07 16:43:32 +0700

    APIs Test Register User&Provider

---
+++ APIs/APIs/Test Listeners/APIsTestUser.groovy
@@ -0,0 +1,60 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.TestObject as TestObject
+
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+
+import internal.GlobalVariable as GlobalVariable
+
+import com.kms.katalon.core.annotation.BeforeTestCase
+import com.kms.katalon.core.annotation.BeforeTestSuite
+import com.kms.katalon.core.annotation.AfterTestCase
+import com.kms.katalon.core.annotation.AfterTestSuite
+import com.kms.katalon.core.context.TestCaseContext
+import com.kms.katalon.core.context.TestSuiteContext
+import java.util.UUID;
+
+import org.junit.After
+
+class APIsTestUser {
+	
+	private RequestObject apiUser = findTestObject('Object Repository/Register/registerUser')
+	
+	def response
+	
+	private void postAPI(){
+		if(true){
+			response = WS.sendRequest(apiUser)
+			WS.verifyResponseStatusCode(response, 200)
+		}
+	}
+	
+	def setHttpBody() {
+		apiUser.setHttpBody('{"name": "Test Automated User","email": "'+generateRandomString()+'@golfja.com","password": "123456","image": "url"}')
+		postAPI()
+	}
+	
+	public String generateRandomString(){
+         
+        String alphabet= "abcdefghijklmnopqrstuvwxyz";
+        String s = "";
+        Random random = new Random();
+        int randomLen = 1+random.nextInt(9);
+        for (int i = 0; i < randomLen; i++) {
+            char c = alphabet.charAt(random.nextInt(26));
+            s+=c;
+        }
+        return s;
+    }
+	
+}
