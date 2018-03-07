37278002d54ffc2d640590def4e4df9379947870
patawee <psgolf16@gmail.com>
2018-03-07 16:42:02 +0700

    Global Variable

--- APIs/APIs/Libs/internal/GlobalVariable.groovy
+++ APIs/APIs/Libs/internal/GlobalVariable.groovy
@@ -16,4 +16,9 @@ import groovy.transform.CompileStatic
 @CompileStatic
 public class GlobalVariable {
 	 
+    /**
+     * <p></p>
+     */
+	public static Object server = 'https://api-co-work.herokuapp.com/'
+	 
 }
