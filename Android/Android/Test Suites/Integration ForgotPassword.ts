d4967eff18354acb2e3262015fa3e95d30dc37e4
patawee <psgolf16@gmail.com>
2018-03-09 16:55:33 +0700

    Integration Test

---
+++ Android/Android/Test Suites/Integration ForgotPassword.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Integration ForgotPassword</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-09T14:53:36</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b744de72-44e1-4ffd-8915-3d648f21a171</testSuiteGuid>
+   <testCaseLink>
+      <guid>a99f86ea-03d2-4f7f-92f7-c818ef9079c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/ForgotPassword_TC01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad19864c-ecde-4481-8e92-40b7e596f04e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/ForgotPassword_TC02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3bf9de3c-ec22-4cb2-a42f-a11ea62356ec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/ForgotPassword_TC03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8958804-08d0-4b57-902f-cdc51a8a5c72</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/ForgotPassword_TC07</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a9597286-5664-4239-9b19-1e51fe041ca0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ForgotPassword/ForgotPassword_TC08</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Android/Android/Test Suites/Integration Login.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Integration Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-09T14:30:37</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>086eba8c-250f-4d74-b5e2-3c864dc83fbe</testSuiteGuid>
+   <testCaseLink>
+      <guid>5db217a9-64bc-46e5-9822-445d84fadba9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEmail/LoginEmail_TC01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ff44701d-3cc5-43f2-bb7b-067a39c88a1b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEmail/LoginEmail_TC02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>97b68e82-e491-4965-b00f-f9d1122b77c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEmail/LoginEmail_TC03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>446d3c4c-7ac8-4350-a61c-ffe1d48a60a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEmail/LoginEmail_TC04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bacf1c09-431c-4756-8b81-eba5407ca8dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEmail/LoginEmail_TC05</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2342fe5f-570a-42eb-b792-c06f8729385e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEmail/LoginEmail_TC06</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5bd76d4b-e42b-471b-8cd0-aeda71e84c7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEmail/LoginEmail_TC07</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
