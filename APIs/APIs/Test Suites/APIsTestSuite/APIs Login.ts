e3a762245d33196bdb3cb63f3f24a1d4c2ef8127
patawee <psgolf16@gmail.com>
2018-03-20 15:59:10 +0700

    TestSuite

---
+++ APIs/APIs/Test Suites/APIsTestSuite/APIs Login.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>APIs Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-20T15:49:01</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>159a5bf3-1218-46f3-9824-1b942ce83983</testSuiteGuid>
+   <testCaseLink>
+      <guid>7f6b1529-dbf2-4c6e-bdec-eab530e88a8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/email_login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>61009be0-fdd5-4368-9194-40faa38acc9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/facebook_login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
