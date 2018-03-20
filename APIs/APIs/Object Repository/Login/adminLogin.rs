ad9d83fd50a5c60ba7dff13359de7e4a0663d7f3
patawee <psgolf16@gmail.com>
2018-03-20 17:07:11 +0700

    Object For Test

--- APIs/APIs/Object Repository/Login/adminLogin.rs
+++ APIs/APIs/Object Repository/Login/adminLogin.rs
@@ -1,19 +1,15 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>registerProvider</name>
+   <name>adminLogin</name>
    <tag></tag>
-   <elementGuidId>92ba893f-70cb-4e3f-a6d7-bbf58ef767d9</elementGuidId>
+   <elementGuidId>2be7400b-3553-47ee-89f0-dc4bdec79181</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <httpBody>{
- &quot;name&quot;:&quot;provider_01&quot;,
- &quot;email&quot;:&quot;asdsddd@provider.test&quot;,
- &quot;password&quot;:&quot;provider_password&quot;,
- &quot;phone&quot;:&quot;080-12345678&quot;,
- &quot;contact&quot;: &quot;123 m.2 pangtest merng chiangmail 54000&quot;,
- &quot;image&quot;: &quot;url_image.image&quot;
-}</httpBody>
+								&quot;email&quot; : &quot;admin@admin.admin&quot;,
+								&quot;password&quot; : &quot;123456&quot;
+								}</httpBody>
    <httpHeaderProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
@@ -22,7 +18,7 @@
       <value>application/json</value>
    </httpHeaderProperties>
    <restRequestMethod>POST</restRequestMethod>
-   <restUrl>https://api-co-work.herokuapp.com/api/register/provider</restUrl>
+   <restUrl>https://api-co-work.herokuapp.com/api/admin-login </restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
